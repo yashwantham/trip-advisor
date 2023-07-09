9bea02c83daca795c2c23bc8b956c7de98f7d65d
Yashwanth <yashwanthyashu166@gmail.com>
2023-07-09 21:11:09 +0530

    initial commit

---
+++ src/pages/homePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import "./HomePage.css";
+
+export function HomePage() {
+    return (
+        <>
+        <div className="homepage-container">
+            <h1>home page</h1>
+        </div>
+        </>
+    )
+}
